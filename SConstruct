8f71ec919bd541956b3dfaa600d2d8207ce66831
Eddie Ringle <eddie@eringle.net>
2011-10-23 14:40:34 -0400

    Basic outline of draft engine api
    Signed-off-by: Eddie Ringle <eddie@eringle.net>

--- SConstruct
+++ SConstruct
@@ -1,5 +1,14 @@
 env = Environment() # initialize the environment
 
 library_path = "contrib/install/lib" # path of contrib libraries
+include_path = "contrib/install/include" # path of contrib includes
+
+# Basic CFLAGS for correctness
+env.Append(CFLAGS=['-std=c99', '-fno-strict-aliasing'])
+# Warning flags
+#env.Append(CFLAGS='-Wall -Wextra -Wstrict-prototypes -Wold-style-definition -Wmissing-prototypes -Wmissing-declarations -Werror=declaration-after-statement')
+
+env.Append(CPPPATH = include_path, LIBPATH = library_path,
+        LIBS = ['cerberus'])
 
 Library('bin/cerberus', Glob('source/*.c'))
---
+++ source/cerberus.h
@@ -0,0 +1,78 @@
+/*
+ * Copyright (c) 2011 Eddie Ringle <eddie@eringle.net>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+ * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#ifndef __included_cerberus_h
+#define __included_cerberus_h
+
+#include <stdio.h>
+#include <stdbool.h>
+
+#include "universal_include.h"
+
+typedef unsigned int u_int;
+
+// Entity structure
+typedef struct {
+    u_int          id;
+    const char    *name;
+    // Data::HashTable<behaviorFunc> behaviors;
+    // Data::HashTable<property_t *> properties;
+
+    struct Entity *parent;
+    // Data::LList<Entity *> children;
+} Entity;
+
+typedef void(*behaviorFunc)(Entity*,float); // Putting this here for now
+
+// Core engine functions (initialize, enable/disable, and quit)
+bool    crb_initialize(u_int _flags);
+bool    crb_enable(u_int _flag);
+bool    crb_disable(u_int _flag);
+bool    crb_quit();
+
+// Logging functions
+void    crb_debug_out(const char *_msg);
+
+// Generic game loop
+bool    crb_loop();
+
+// Input functions
+int     crb_handle_input();
+
+// Graphics functions
+bool    crb_gfx_setup_window(int _width, int _height, int _bpp, u_int _flags);
+int     crb_gfx_tex_from_image(const char *_image);
+
+// Entity-specific functions
+int     crb_entity_create();
+Entity *crb_entity_get(int _id);
+bool    crb_entity_set_prop(const char *_name, void *_value);
+void   *crb_entity_get_prop(const char *_name);
+bool    crb_entity_add_behavior(const char *_name, behaviorFunc _func);
+bool    crb_entity_remove_behavior(const char *_name);
+bool    crb_entity_destroy(int _id);
+
+#endif // __included_cerberus_h
